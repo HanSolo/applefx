9e90d276516d556d8d466faf019224c35c586778
Gerrit Grunwald <han.solo@mac.com>
2022-03-08 13:29:52 +0100

    Updated dependencies

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -15,6 +15,11 @@ module eu.hansolo.applefx {
     requires transitive eu.hansolo.toolbox;
     requires transitive eu.hansolo.toolboxfx;
 
+    opens eu.hansolo.applefx to eu.hansolo.jdktools, eu.hansolo.toolbox, eu.hansolo.toolboxfx;
+    opens eu.hansolo.applefx.tools to eu.hansolo.jdktools, eu.hansolo.toolbox, eu.hansolo.toolboxfx;
+    opens eu.hansolo.applefx.event to eu.hansolo.jdktools, eu.hansolo.toolbox, eu.hansolo.toolboxfx;
+    opens eu.hansolo.applefx.fonts to eu.hansolo.jdktools, eu.hansolo.toolbox, eu.hansolo.toolboxfx;
+
     exports eu.hansolo.applefx;
     exports eu.hansolo.applefx.tools;
     exports eu.hansolo.applefx.event;
