ef1117af449146b9f5dc9ba1dfee6aef8f1efd32
Gerrit Grunwald <han.solo@mac.com>
2023-08-18 12:43:35 +0200

    Some fixes

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,10 +5,10 @@ module eu.hansolo.applefx {
     requires java.desktop;
 
     // Java-FX
-    requires transitive javafx.base;
-    requires transitive javafx.graphics;
-    requires transitive javafx.controls;
-    requires transitive javafx.swing;
+    requires javafx.base;
+    requires javafx.graphics;
+    requires javafx.controls;
+    requires javafx.swing;
 
     // 3rd party
     requires transitive eu.hansolo.jdktools;
